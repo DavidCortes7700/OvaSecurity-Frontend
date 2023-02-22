c80abde1bff26832304cee46d68cbfd192891827
DavidCortes7700 <davidcortes7700@gmail.com>
2023-02-21 22:27:04 -0500

    agregar cards y modal de carga de material

--- src/pages/teacher/ActivitiesTeacher.jsx
+++ src/pages/teacher/ActivitiesTeacher.jsx
@@ -1,11 +1,41 @@
-
+import React, { useState } from 'react';
 import MenuBar from 'components/MenuBar';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ActivitiesCards from 'components/ActivitiesCards';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Modal from 'pages/teacher/ActivitiesTeacherAdd';
 
 function ActivitiesTeacher() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <MenuBar/>
-    
+    <>
+      <MenuBar/>
+      <div className='card-container'>
+        <ActivitiesCards/>
+        <ActivitiesCards/>
+        <ActivitiesCards/>
+        <ActivitiesCards/>
+        <ActivitiesCards/>
+      </div>
+      <Fab 
+        color="primary" 
+        aria-label="add" 
+        style={{position: 'fixed', bottom: '20px', right: '20px', backgroundColor: 'green', color: 'white'}}
+        onClick={openModal}
+      >
+        <AddIcon />
+      </Fab>
+      <Modal isOpen={isModalOpen} onClose={closeModal} />
+    </>
   );
 }
 
